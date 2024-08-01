e2c491a97ea1509131824f5c4d986300fee61dfb
greg_dorian <greg_dorian@yahoo.com>
2024-08-01 17:00:08 -0500

    modulo3

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,12 +11,16 @@ urlpatterns = [
     # # path for registration
 
     # path for login
+    path(route='login', view=views.login_user, name='login'),
     # path('login/', TemplateView.as_view(template_name="index.html")),
 
 
     # path for dealer reviews view
-     path(route='login', view=views.login_user, name='login'),
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
 
      # path for add a review view
-
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+        path(route='add_review', view=views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
         path('about/', TemplateView.as_view(template_name="About.html")),
         path('contact/', TemplateView.as_view(template_name="Contact.html")),
         path('login/', TemplateView.as_view(template_name="index.html")),
+        path('register/', TemplateView.as_view(template_name="index.html")),
+        path('dealers/', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
